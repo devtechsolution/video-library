9a313704bf9b85af5a9a4bcc769860d73f08ef60
devtechsolution <srivastvadevzone@gamil.com>
2022-05-08 12:59:48 +0530

    01 Initial commit

---
+++ video-library/backend/src/main/java/org/as/devtechsolution/VideoLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoLibraryApplication.class, args);
+	}
+
+}
